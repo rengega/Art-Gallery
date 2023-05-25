17dc4a9d5365becc1c5f3e5b24ac4550050e4341
rengega <renigega@outlook.it>
2023-05-25 19:38:22 +0200

    added login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . views import signup
+from .forms import LoginForm
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', signup, name = 'signup')
+    path('signup/', signup, name = 'signup'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'accounts/login.html', authentication_form = LoginForm), name = 'login'),
     ]
