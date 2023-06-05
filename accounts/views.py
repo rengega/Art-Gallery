4af1c974a493f06e5013bc830daa9502726de2a0
rengega <renigega@outlook.it>
2023-06-05 20:38:00 +0200

    fixed the ridirecting error on signup

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts/login')
+            return redirect('accounts:login')
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
