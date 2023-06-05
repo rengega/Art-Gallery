79d435d85655164b70b5a2fc8e537dd04fa74522
rengega <renigega@outlook.it>
2023-06-05 18:00:02 +0200

    added env variables

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^nmll=wx=be@z91f!76ebe3i2-%4%-@bba8snn)iet+zh+u^!+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,12 +103,12 @@ DATABASES = {
 
 
 # Use Wasabi as the default file storage
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'I2A7KT82H3VONTMDNKJT'
-AWS_SECRET_ACCESS_KEY = 'lv1Ht6NRnqAzXUEDyBIlHMz1iLABfqTbi1ETlfXL'
-AWS_STORAGE_BUCKET_NAME = 'artgallery'
-AWS_S3_ENDPOINT_URL = 'https://s3.eu-central-1.wasabisys.com'
-AWS_S3_REGION_NAME = 'eu-central-1'  # Replace with the appropriate region
+DEFAULT_FILE_STORAGE = env('DEFAULT_FILE_STORAGE')
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env(AWS_SECRET_ACCESS_KEY)
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
