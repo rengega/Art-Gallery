81dff49f0c6408b71aed781339d4ea1a0a293847
rengega <renigega@outlook.it>
2023-06-05 18:09:09 +0200

    added domanin variable in env

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -66,9 +66,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-#CSRF_TRUSTED_ORIGINS = ['https://art-gallery-production-5cb9.up.railway.app']
-#CSRF_COOKIE_DOMAIN = 'art-gallery-production-5cb9.up.railway.app'
-ROOT_URLCONF = 'art_gallery.urls'
+CSRF_TRUSTED_ORIGINS = ['https://art-gallery-production-5cb9.up.railway.app']
+CSRF_COOKIE_DOMAIN = env('CSRF_COOKIE_DOMAIN')
+ROOT_URLCONF = env('ROOT_URLCONF')
 
 TEMPLATES = [
     {
